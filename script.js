da808e3f35cdb3be3aa59dcab559ea8ff270edc4
Vadim <cwood576@yandex.ru>
2022-06-06 21:20:35 +0300

    Добавить попап и скрипты

---
+++ script.js
@@ -0,0 +1,33 @@
+let popup = document.querySelector('.popup');
+let name = document.querySelector('.profile__name');
+let status = document.querySelector('.profile__status')
+let popupName = document.querySelector('.popup__name')
+let popupStatus = document.querySelector('.popup__status')
+let editButton = document.querySelector('.profile__edit');
+let closePopupButton = document.querySelector('.popup__close');
+let sendFormButton = document.querySelector('.popup__button');
+
+popupName.value = name.innerText;
+popupStatus.value = status.innerText;
+
+
+function openPopup() {
+    popup.classList.add('popup_opened');
+}
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+    popupName.value = name.innerText;
+    popupStatus.value = status.innerText;
+}
+
+function saveProfile() {
+    name.innerText = popupName.value;
+    status.innerText = popupStatus.value;
+    closePopup();
+}
+
+
+sendFormButton.addEventListener("click", saveProfile);
+editButton.addEventListener("click", openPopup);
+closePopupButton.addEventListener("click", closePopup);
